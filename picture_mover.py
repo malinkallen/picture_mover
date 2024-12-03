fcbbdf44e84ed83a8e0cbc908231d189d43b00c3
malinkallen <malin.anno2008@gmail.com>
2024-12-03 16:18:46 +0100

    Change order of functions

--- picture_mover.py
+++ picture_mover.py
@@ -25,10 +25,6 @@ def get_files_in_db(db_file):
 def get_filenames_from_table(cursor, table_name):
     return set(map(operator.itemgetter(0), cursor.execute("SELECT filename FROM " + table_name).fetchall()))
 
-def ensure_directory(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
 def archive_files(files, src_dir, archive_dir):
     for src in files:
         target = src.replace(src_dir, archive_dir)
@@ -36,6 +32,10 @@ def archive_files(files, src_dir, archive_dir):
         target_dir = '/'.join(target_parts[:-1])
         ensure_directory(target_dir)
         os.rename(src, target)
+
+def ensure_directory(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 home = os.environ["HOME"]
 db_file = home + "/.local/share/shotwell/data/photo.db"
